1a5659186d13d50cb4ba90bcb95945076117e6d1
sphilmoon <sphilmoon@gmail.com>
2020-11-03 11:35:09 +0900

    updated few comments and fixed typos.

--- heatmap/heatmap_vs_vac.py
+++ heatmap/heatmap_vs_vac.py
@@ -13,26 +13,26 @@ tags = [] # leave as a blank for data to be appended.
 head= lines[0].strip().split(",") # must use comma for csv files.
 
 for line in lines[1:]: # excluding the header. Starting from the 2nd row.
-        parse = line.strip().split(",")
-            values.append([float(i) for i in parse[-3:]])
-                tags.append(parse[:-3])
-                    #print(parse[:-4])
+    parse = line.strip().split(",")
+    values.append([float(i) for i in parse[-3:]])
+    tags.append(parse[:-3])
+    #print(parse[:-4])
 
-                    tags = np.asarray(tags)
-                    v_gene = tags[:, 1] # passes the tuple.
-                    #print(tags[1], values[1])
-                    np_values = np.asarray(values)
-                    #code.interact(local=dict(globals(), **locals())) # for interactive mode to monitor what has been processed.
+tags = np.asarray(tags)
+v_gene = tags[:, 1] # passes the tuple.
+#print(tags[1], values[1])
+np_values = np.asarray(values)
+#code.interact(local=dict(globals(), **locals())) # for interactive mode to monitor what has been processed.
 
-                    np_values = pd.DataFrame(np_values,columns=list(head[-3:]), index = v_gene)
+np_values = pd.DataFrame(np_values,columns=list(head[-3:]), index = v_gene)
 
-                    #print(type(np_values[0][0]))
-                    plt.figure(1, figsize=(25, 25))
+#print(type(np_values[0][0]))
+plt.figure(1, figsize=(25, 25))
 
-                    ax = sns.heatmap(np_values,cmap="RdYlGn") #YlGnBu
+ax = sns.heatmap(np_values,cmap="RdYlGn") #YlGnBu
 
-                    plt.ylabel("IgH+L V genes vs VAC", fontsize=22)
-                    plt.xlabel("Conditions", fontsize=22)
+plt.ylabel("IgH+L V genes vs VAC", fontsize=22)
+plt.xlabel("Conditions", fontsize=22)
 
-                    ax.plot()
-                    plt.savefig("clonotype_vs_vac.png")
+ax.plot()
+plt.savefig("clonotype_vs_vac.png")
