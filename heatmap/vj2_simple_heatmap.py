3ab2796f95390623dd8ac7bb361a0597152e2c41
sphilmoon <sphilmoon@gmail.com>
2020-12-11 08:06:13 +0900

    adjusted color bar size and bolded fonts.

--- heatmap/vj2_simple_heatmap.py
+++ heatmap/vj2_simple_heatmap.py
@@ -6,15 +6,15 @@ import seaborn as sns
 df = pd.read_csv('/Users/philmoon/Documents/rna_pracitce_mac/vdj_cellranger/csv/IgHJ2_summary_113020_PM.csv', index_col = 'IGHV')
 
 # outputting figure and axes using matplotlib
-fig, ax = plt.subplots(figsize = (20, 20))
+fig, ax = plt.subplots(figsize = (6, 18))
 
 # creating colormap/heatmap using seaborn
-sns.heatmap(df, cmap = "vlag")
+sns.heatmap(df, cmap = "vlag", square = True, linewidth = 0.3, cbar_kws = {"shrink": 0.4})
 
 # labeling titles and axes.
-ax.set_title("V-J2 Heavy Chains", fontsize = 22)
-plt.ylabel("IgHV", fontsize = 22)
-plt.xlabel("IgHJ2", fontsize = 22)
+ax.set_title("V-J2 Heavy Chains", fontsize = 22, fontweight = "bold")
+plt.ylabel("IgHV", fontsize = 22 ,fontweight = "bold")
+plt.xlabel("IgHJ2", fontsize = 22, fontweight = "bold")
 
 # saving the plot
 plt.savefig("vj2_heavy_wt.png")
