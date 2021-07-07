ecd3c7aba567051391a008216b79ee0ca633308f
sphilmoon <sphilmoon@gmail.com>
2021-07-07 10:44:50 +0900

    10x cellranger vdj standard command.

---
+++ commands/run_cellranger_command.sh
@@ -0,0 +1,4 @@
+cellranger vdj --id=output_wt \
+        --reference=/cellranger_wt/reference/refdata-cellranger-vdj-GRCm38-alts-ensembl-4.0.0 \
+        --fastqs=/cellranger_wt/rawdata/wt \
+        --sample=wt
